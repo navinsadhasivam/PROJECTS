7f459df699130a69bed09e619bc7505246ac2d7b
navinsadhasivam <36471837+navinsadhasivam@users.noreply.github.com>
2018-03-13 07:35:19 +0530

    Create mednum.py

---
+++ mednum.py
@@ -0,0 +1,19 @@
+a = input()
+b = input()
+c = input()
+if a > b:
+    if a < c:
+        median = a
+    elif b > c:
+        median = b
+    else:
+        median = c
+else:
+    if a > c:
+        median = a
+    elif b < c:
+        median = b
+    else:
+        median = c
+
+print("The median is", median)
