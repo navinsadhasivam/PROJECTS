082acd459c1cc8608236e6d819b918da372ca07f
navinsadhasivam <36471837+navinsadhasivam@users.noreply.github.com>
2018-03-19 18:12:14 +0530

    Create maxtwo.py

---
+++ maxtwo.py
@@ -0,0 +1,13 @@
+n=int(input("Enter  N value:"))
+k=int(input("Enter  N value:"))
+print("Enter the first array:")
+a=[]
+for x in range(n):
+
+	m=int(input())
+	a.append(m)
+print("Enter K array:")
+for x in range(k):
+	m=int(input())
+	a.append(m)
+	print(max(a)
