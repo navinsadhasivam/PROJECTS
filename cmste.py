8cf1f66cb24b613419c6fd805f4cad9b6cb08a2e
navinsadhasivam <36471837+navinsadhasivam@users.noreply.github.com>
2018-04-02 07:55:18 +0530

    Create cmste.py

---
+++ cmste.py
@@ -0,0 +1,12 @@
+num = int(input())
+if num > 1:
+    for i in range(2, num):
+        if (num % i) == 0:
+            print("Yes")
+            break
+    else:
+        print("No")
+elif num == 0 or 1:
+    print(" a neither prime NOR composite number")
+else:
+    print("Yes")
