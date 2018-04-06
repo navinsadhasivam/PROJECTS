dfa474ffcbc7acfd7d7f4ba05215905a21a7ed3a
navinsadhasivam <36471837+navinsadhasivam@users.noreply.github.com>
2018-04-06 06:38:22 +0530

    Create aply45.py

---
+++ aply45.py
@@ -0,0 +1,8 @@
+p=list(map(int,input().split(' ')))
+s=int(p[1]**0.5)
+if float(s)==float(p[1]**0.5):
+    if int(p[1]**(0.5))==(p[0]//4):
+        print 'Yes'
+    else:
+        print 'No'
+else: print 'No'
